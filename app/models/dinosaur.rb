6aeb0fcbd93e32c2a21ece5d9ef6d10a46191ccd
Manon Oriol <oriol.manon@gmail.com>
2020-12-17 16:22:51 +0100

    validations ok

--- app/models/dinosaur.rb
+++ app/models/dinosaur.rb
@@ -1,3 +1,7 @@
 class Dinosaur < ApplicationRecord
   belongs_to :park
+
+  validates :name, uniqueness: :true, presence: :true
+  validates :description, presence: :true
+  validates :image_url, presence: :true
 end
