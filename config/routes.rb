ae26fa8385f9ac55ab790c90e908b033d61951b4
Manon Oriol <oriol.manon@gmail.com>
2020-12-18 11:59:53 +0100

    controller index ok + seed done

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  root to: 'pages#home'
+  root to: 'parks#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :parks, only [:index, :show, :new, :create] do
-    resources :dinosaurs, only [:create]
+  resources :parks, only: [:index, :show, :new, :create] do
+    resources :dinosaurs, only: [:create]
   end
-  resources :dinosaurs, only [:destroy]
+  resources :dinosaurs, only: [:destroy]
 end
