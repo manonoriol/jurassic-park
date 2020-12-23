e1ae16c1b63ba32c549fcfd1c5997be489bf4624
Manon Oriol <oriol.manon@gmail.com>
2020-12-23 19:41:20 +0100

    added new show to controller and icons on frontpage

--- app/controllers/parks_controller.rb
+++ app/controllers/parks_controller.rb
@@ -2,4 +2,28 @@ class ParksController < ApplicationController
   def index
     @parks = Park.all
   end
+
+  def new
+    @park = Park.new
+  end
+
+  def create
+    @park = Park.new(park_params)
+    if @park.save
+      redirect_to park_path(@park)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @park = Park.find(params[:id])
+  end
+
+  private
+
+  def park_params
+    params.require(:park).permit(:name, :banner_url)
+  end
 end
+
